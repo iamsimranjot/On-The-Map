b7e2e87fa903523241f42da5b7c3739cd8c9155e
iamsimranjot <iamsimranjot@gmail.com>
2016-12-25 00:27:56 +0530

    New Project Created.

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by SimranJot Singh on 25/12/16.
+//  Copyright © 2016 SimranJot Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
