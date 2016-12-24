e296176e962178c81fbcaac54be82d3317ec010a
iamsimranjot <iamsimranjot@gmail.com>
2016-12-25 02:21:29 +0530

    App Assets Added.

--- On The Map/AppDelegate.swift
+++ On The Map/AppDelegate.swift
@@ -10,9 +10,12 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    //MARK: Properties
+    
     var window: UIWindow?
 
+    //MARK: UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
