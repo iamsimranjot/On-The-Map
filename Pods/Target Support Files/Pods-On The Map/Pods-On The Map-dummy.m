e5d94d0920578167f6ab08336803cc50920d9965
iamsimranjot <iamsimranjot@gmail.com>
2016-12-25 14:08:35 +0530

    Facebook SDK Added through Pods.

---
+++ Pods/Target Support Files/Pods-On The Map/Pods-On The Map-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_On_The_Map : NSObject
+@end
+@implementation PodsDummy_Pods_On_The_Map
+@end
---
+++ Pods/Target Support Files/Pods-On The Map/Pods-On The Map-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_On_The_MapVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_On_The_MapVersionString[];
+
