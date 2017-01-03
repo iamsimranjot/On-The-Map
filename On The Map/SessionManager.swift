6cc554996ce075d249c627ec7db9ff772cc89cdb
iamsimranjot <iamsimranjot@gmail.com>
2017-01-03 11:00:24 +0530

    Session Manager Class Created.

---
+++ On The Map/SessionManager.swift
@@ -0,0 +1,29 @@
+//
+//  SessionManager.swift
+//  On The Map
+//
+//  Created by SimranJot Singh on 25/12/16.
+//  Copyright © 2016 SimranJot Singh. All rights reserved.
+//
+
+import Foundation
+
+//MARK: HTTP Request Method Enum
+
+enum HTTPMethod: String {
+    case GET, POST, PUT, DELETE
+}
+
+//MARK:
+
+struct APIUrlData {
+    let scheme: String
+    let host: String
+    let path: String
+}
+
+class SessionManager {
+    
+    //MARK: Properties
+    
+}
