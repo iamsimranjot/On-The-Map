eb8680d4e693b5c655efb0b7680ac0184e165e6b
Simranjot Singh <iamsimranjot@simranjot-2.local>
2017-01-10 18:05:27 +0530

    Student Location Cell Added.

---
+++ StudentLocationCell.swift
@@ -0,0 +1,24 @@
+//
+//  StudentLocationCell.swift
+//  On The Map
+//
+//  Created by Simranjot Singh on 10/01/17.
+//  Copyright © 2017 SimranJot Singh. All rights reserved.
+//
+
+import UIKit
+
+class StudentLocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
