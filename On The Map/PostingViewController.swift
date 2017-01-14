768ee2c72f157bff4ba1fc8c8daf50750e96579c
iamsimranjot <iamsimranjot@gmail.com>
2017-01-14 12:57:23 +0530

    Posting View Controller Made

---
+++ On The Map/PostingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostingViewController.swift
+//  On The Map
+//
+//  Created by SimranJot Singh on 14/01/17.
+//  Copyright © 2017 SimranJot Singh. All rights reserved.
+//
+
+import UIKit
+
+class PostingViewController: UIViewController {
+    
+    //MARK: Outlets
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
