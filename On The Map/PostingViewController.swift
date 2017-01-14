7035a55313088c78ceca752d9091d91b30896280
iamsimranjot <iamsimranjot@gmail.com>
2017-01-14 13:02:50 +0530

    Connection Outlets Connected. (Posting View Controller)

--- On The Map/PostingViewController.swift
+++ On The Map/PostingViewController.swift
@@ -7,16 +7,29 @@
 //
 
 import UIKit
+import MapKit
 
 class PostingViewController: UIViewController {
     
     //MARK: Outlets
     
+    @IBOutlet weak var topSection: UIView!
+    @IBOutlet weak var topSectionLabelView: UIView!
+    @IBOutlet weak var mediaURLTextField: UITextField!
+    @IBOutlet weak var middleSection: UIView!
+    @IBOutlet weak var mapKitView: UIView!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var bottomSection: UIView!
+    @IBOutlet weak var submitView: UIView!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var findButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
